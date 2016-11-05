8fed753154b3e3744811d0c9eaf0ce1429d89289
AliKheyrollahi <aliostad@AliKheylahisMBP.home>
2016-11-05 16:26:42 +0000

    adding the bot

---
+++ album_genre_bot.py
@@ -0,0 +1,68 @@
+import twitter
+import eval_single
+import click
+import re
+import os
+from datetime import datetime, timedelta
+
+timestamp_filename = '__last_timestamp__'
+twitterId = '@AlbumGenreBot'
+
+def get_time_stamp():
+
+  if os.path.exists(timestamp_filename):
+    return datetime.strptime(open(timestamp_filename, mode='r').read(), '%Y-%m-%d %H:%M:%S')
+
+  else:
+    return datetime.now() - timedelta(days=7)
+
+def save_time_stamp(dt):
+  open(timestamp_filename, 'w').write(dt.strftime('%Y-%m-%d %H:%M:%S'))
+
+def clean_text(tweet_mention_text):
+  return re.sub('@\w+', '', tweet_mention_text).strip()
+
+def predict(text):
+  return eval_single.get_genre(text)
+
+def parse_twitter_date(s):
+  return datetime.strptime(s, '%a %b %d %H:%M:%S +0000 %Y')
+
+@click.command()
+@click.option('--consumer_key', help='Twitter consumer key')
+@click.option('--consumer_secret', help='Twitter consumer secret')
+@click.option('--token_key', help='Twitter token key')
+@click.option('--token_secret', help='Twitter token secret')
+def respond(consumer_key, consumer_secret, token_key, token_secret):
+  if consumer_key is None:
+    print 'usage: python album_genre_bot.py <consumer_key> <consumer_secret> <token_key> <token_secret>'
+    return
+
+  api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=token_key,
+                      access_token_secret=token_secret)
+
+  original_timestamp = get_time_stamp()
+  new_timestamp = original_timestamp
+  for m in api.GetMentions():
+    dt = parse_twitter_date(m.created_at)
+    new_timestamp = max(dt, new_timestamp)
+    if dt > original_timestamp and m.text.startswith(twitterId):
+      genre = predict(clean_text(m.text))
+      status = api.PostUpdate(
+        in_reply_to_status_id=m.id,
+        status='@{} {}'.format(m.user.screen_name, genre))
+      print m.text,
+      print ' ==> ' + genre
+
+  save_time_stamp(new_timestamp)
+
+if __name__ == '__main__':
+  print respond()
+
+
+
+
+
+
--- eval_single.py
+++ eval_single.py
@@ -3,15 +3,9 @@ from __future__ import unicode_literals
 import tensorflow as tf
 import numpy as np
 import os
-import time
-import datetime
 import data_loader
-from text_cnn import TextCNN
 from tensorflow.contrib import learn
-from flask import Flask, jsonify, request, send_from_directory
-import logging
-from logging import FileHandler
-import time
+
 
 # Parameters
 # ==================================================
@@ -28,56 +22,18 @@ tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on 
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
 
 FLAGS.checkpoint_dir = './runs/1477424694/checkpoints/' # 1477424694 1477602229
 # Map data into vocabulary
 vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
 vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
 
-print("\nEvaluating...\n")
-
 # Evaluation
 # ==================================================
 checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
 graph = tf.Graph()
 
-
-t = time.time()
-if __name__ == '__main__':
-  print("Time is now {}".format(t))
-
-app = Flask(__name__)
-application = app
-
-try:
-  file_handler = FileHandler("album_genre_api.log", "a")
-  file_handler.setLevel(logging.WARNING)
-  app.logger.addHandler(file_handler)
-
-  if __name__ == '__main__':
-    print("Took {}".format(time.time() - t))
-except:
-  if __name__ == '__main__':
-    print("could not start file logging")
-
-@app.route('/', methods=['GET'])
-def browse_default():
-  try:
-    return send_from_directory('ui', 'index.html')
-  except Exception as e:
-    return e.message
-
-@app.route('/<path:path>', methods=['GET'])
-def staticx(path):
-   return send_from_directory('ui', path)
-
-
-@app.route('/api/v1/album/genre', methods=['GET'])
-def get_genre():
+def get_genre(album_or_phrase):
 
   try:
 
@@ -92,8 +48,7 @@ def get_genre():
         saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
         saver.restore(sess, checkpoint_file)
 
-        albums = request.args.get('albums')
-        x_raw = albums.split(',')
+        x_raw = [album_or_phrase]
         all_predictions = []
         x_test = np.array(list(vocab_processor.transform(x_raw)))
 
@@ -111,7 +66,7 @@ def get_genre():
             batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
             all_predictions = np.concatenate([all_predictions, batch_predictions])
 
-        return jsonify({'results': map(lambda x: data_loader.genre_ids[int(x)], all_predictions)})
+        return data_loader.genre_ids[int(all_predictions[0])]
 
   except Exception as e:
     print e
